13b240b579d205247e5b4638da210a81479e9dfa
Dursun Selin <dursunseline@gmail.com>
2021-11-04 17:49:59 +0100

    first commit

---
+++ front/js/script.js
@@ -0,0 +1,23 @@
+// Fonction pour requêter l'api avec fetch
+fetch("http://localhost:3000/api/products")
+    // Récupération de la réponse émise
+    .then(response => response.json())
+    .then((data) =>{ 
+
+        // Boucle pour parcourir la réponse émise 
+        data.forEach(function(data){
+            let div = document.createElement("div");
+            div.classList.add("items");
+
+            //Ajout des éléments dans le DOM
+             document.getElementById("items")
+            items.innerHTML += ` <a href="${data._id}">
+            <article>
+              <img src="${data.imageUrl}" alt="${data.altTxt}">
+              <h3 class="productName">${data.name}</h3>
+              <p class="productDescription">${data.description}</p>
+            </article>
+          </a> `
+        });
+    })
+    .catch(error => console.log(error));
