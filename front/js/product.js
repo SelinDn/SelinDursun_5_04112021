cd88c46d2e7e40730af6ba657433bcea61f15e80
Dursun Selin <dursunseline@gmail.com>
2021-11-12 12:18:33 +0100

    Ajout des conditions id et couleurs

--- front/js/product.js
+++ front/js/product.js
@@ -36,45 +36,57 @@ fetch(`http://localhost:3000/api/products/${recoveryId}`)
         }
         //Cibler le bouton "Ajouter au panier" afin de pouvoir créer un événement
         const btnPanier = document.getElementById("addToCart");
-            btnPanier.addEventListener("click", (e)=>{
-                //Pour annuler le comportement par défaut du bouton 
-                e.preventDefault();
-                //Création d'une constante avec comme objet les valeurs des produits
-                const productValue = {
-                    name: data.name,
-                    id: recoveryId,
-                    price: data.price,
-                    description: data.description,
-                    alt: data.altTxt,
-                    color: document.getElementById("colors").value,
-                    quantity: document.getElementById("quantity").value,
-                    image: data.imageUrl,
-                }
-                productValue;
-                console.log(productValue);
+        btnPanier.addEventListener("click", (e)=>{
+            //Pour annuler le comportement par défaut du bouton 
+            e.preventDefault();
+            //Création d'une constante avec comme objet les valeurs des produits
+            const productValue = {
+                name: data.name,
+                id: recoveryId,
+                price: data.price,
+                description: data.description,
+                alt: data.altTxt,
+                color: document.getElementById("colors").value,
+                quantity: document.getElementById("quantity").value,
+                image: data.imageUrl,
+            }
+            productValue;
+            console.log(productValue);
 
-                /*Vérification pour savoir si le localStorage comporte une clé
-                JSON.parse permet de transformer un fichier JSON en objet JS */
-                let addProductToLocalStorage = JSON.parse(localStorage.getItem("Kanap"));
+            /*Vérification pour savoir si le localStorage comporte une clé
+            JSON.parse permet de transformer un fichier JSON en objet JS */
+            let addProductToLocalStorage = JSON.parse(localStorage.getItem("Kanap"));
+            console.log(addProductToLocalStorage);
+            /*Dans le cas de produits avec même id et couleur présents dans le panier 
+            JSON.stringify permet de transformer un objet JS en fichier JSON
+            La méthode find permet de trouver le premier élément en respectant la condition donnée,
+            ici si un produit a le même id et la même couleur, on incrémente juste la quantité */
+            if (addProductToLocalStorage){
+                let resultProduct = addProductToLocalStorage.find(
+                    (element) => element.id === recoveryId && element.color === document.getElementById("colors").value);
+                if (resultProduct) {
+                    let result = parseInt(productValue.quantity) + parseInt(resultProduct.quantity);
+                    resultProduct.quantity = result;
+                    localStorage.setItem("Kanap", JSON.stringify(addProductToLocalStorage));
+                    console.log(addProductToLocalStorage);
+                    } 
+                    /*Dans le cas où la condition find est false 
+                    Pour pouvoir ajouter d'autres produits au localStorage */
+                    else {
+                        addProductToLocalStorage.push(productValue);
+                        localStorage.setItem("Kanap", JSON.stringify(addProductToLocalStorage));
+                    }
+                }
+            //Dans le cas où le panier est vide, création d'un array qu'on transmet au localStorage
+            else{
+                addProductToLocalStorage = [];
+                addProductToLocalStorage.push(productValue);
+                localStorage.setItem("Kanap", JSON.stringify(addProductToLocalStorage));
                 console.log(addProductToLocalStorage);
-                /*Dans le cas de produits présents dans le panier 
-                JSON.stringify permet de transformer un objet JS en fichier JSON */
-                if(addProductToLocalStorage){
-                    addProductToLocalStorage.push(productValue);
-                    localStorage.setItem("Kanap", JSON.stringify(addProductToLocalStorage));
-                    console.log(addProductToLocalStorage);
-                }
-                //Dans le cas où le panier est vide
-                else{
-                    addProductToLocalStorage = [];
-                    addProductToLocalStorage.push(productValue);
-                    localStorage.setItem("Kanap", JSON.stringify(addProductToLocalStorage));
-                    console.log(addProductToLocalStorage);
-                }
-            })   
+            }       
         })
-.catch(error => console.log(error));
+    })
+    .catch(error => console.log(error));
 
  
 
-    
